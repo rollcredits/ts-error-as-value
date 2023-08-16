b0c0c4ef422b87693a3247f075c20bec14654120
bigbizze <hpcngmoh@gmail.com>
2023-08-16 09:15:34 -0400

    Updating interface

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -3,9 +3,7 @@ type Success<T> = import(".").Success<T>;
 type Failure<E extends Error> = import(".").Failure<E>;
 type Result<T = void, E extends Error = Error> = import(".").Result<T, E>;
 declare class ResultIs {
-  // If no argument is given to success, assume the success result type is void
-  static success: () => Success<void>;
-  static success: <T>(data: T) => Success<T>;
+  static success: <T = void>(data?: T) => Success<T>;
   static failure: <E extends Error>(failure: E) => Failure<E>;
 }
 
